a3f6773b0fadf3f474d1c1173019d65d65a13848
Senbonzakura <shadowmeld055@gmail.com>
2025-04-24 15:39:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import requests
 import json
-response = requests.get("http://www.omdbapi.com/?apikey=505480d7&s=joker")
-if response.status_code == 200:
-    print(response.json())  # {'name': 'michael', 'age': 40, 'count': 12345}
+# response = requests.get("http://www.omdbapi.com/?apikey=505480d7&s=joker")
+# if response.status_code == 200:
+#     print(response.json())  # {"name": "michael", "age": 40, "count": 12345}
+
+while True:
+    command = input('Enter command (or "exit" to quit): ')
+    if command.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    response = requests.get("http://www.omdbapi.com/?apikey=505480d7&s=" + command)
+    if response.status_code == 200:
+        data = response.json()
+        for movie in data.get("Search"): #  для фильмов в список вытащить пbreоиск
+            print(f" {movie['Title']} ({movie['Year']})") # выводим пользователю его результаты
 
